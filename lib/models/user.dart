5f2fa60b9be9eb88a5dfc1dfba103017c856ad04
Alexander Gutiérrez <alexander.gutv@gmail.com>
2020-05-18 18:41:35 -0400

    User singleton with secure storage

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,16 +1,52 @@
 
 
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 class User {
 
-  final int id;
+  final String id;
   final String username;
   final String email;
   final String token;
 
-  User(this.id, this.username, this.email, this.token);
+  static User _instance;
 
-  User fromJson(Map user){
-    return null;
+  User({this.id, this.username, this.email, this.token}) {
+    _instance = this;
+  }
+
+  static User getInstance() {
+    if (_instance == null) {
+      getData();
+    }
+    return _instance;
+  }
+
+  static getData() async {
+    /*
+     */
+    final storage = new FlutterSecureStorage();
+
+    Map<String, String> values = await storage.readAll();
+    if (values != null){
+      _instance = User(
+          id: values['id'],
+          username: values['username'],
+          email: values['email'],
+          token: values['token']
+      );
+    }
+  }
+
+  saveData() async {
+    /*
+     */
+    final storage = new FlutterSecureStorage();
+
+    await storage.write(key: 'id', value: id);
+    await storage.write(key: 'username', value: username);
+    await storage.write(key: 'email', value: email);
+    await storage.write(key: 'token', value: token);
   }
 
 }
